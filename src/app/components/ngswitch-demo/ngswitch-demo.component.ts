85b648bb8b5f0ee83454fb69cde4f42a0bac6f4a
SD <sashaluckyman@gmail.com>
2019-05-29 22:31:12 -0700

    ngswitch component created

---
+++ src/app/components/ngswitch-demo/ngswitch-demo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ngswitch-demo',
+  templateUrl: './ngswitch-demo.component.html',
+  styleUrls: ['./ngswitch-demo.component.css']
+})
+export class NgswitchDemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
